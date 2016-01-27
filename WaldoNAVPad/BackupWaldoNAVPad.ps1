7db9720d7643c667cef4851e6ec0ac210a7ac917
waldo <github@waldo.be>
2016-01-28 00:09:06 +0100

    InitialVersion
    Nothing works yet

---
+++ WaldoNAVPad/BackupWaldoNAVPad.ps1
@@ -0,0 +1,9 @@
+﻿$Name = 'WaldoNAVPad'
+$BackupPath = 'C:\Users\Administrator\Dropbox\GitHub\Waldo.NAV\WaldoNAVPad'
+
+
+$CreatedITems = Backup-NAVApplicationObjects -BackupOption OnlyModified -ServerInstance DynamicsNAV90 -BackupPath $BackupPath -Name $Name
+
+$CreatedITems |where Name -like '*.txt' | Split-NAVApplicationObjectFile -Destination "$BackupPath\Split\"
+
+start $BackupPath
