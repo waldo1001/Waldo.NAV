19f93fa6f745e01bb8bda3364d608a86ea06205a
waldo <github@waldo.be>
2016-01-28 17:50:29 +0100

    InitialVersion
    Still Doesn't Work

--- WaldoNAVPad/BackupWaldoNAVPad.ps1
+++ WaldoNAVPad/BackupWaldoNAVPad.ps1
@@ -1,9 +1,12 @@
 ﻿$Name = 'WaldoNAVPad'
+$DEVInstance = 'WaldoNAVPad_DEV'
+$ORIGInstance = 'DynamicsNAV90'
+$WorkingFolder = 'C:\_Workingfolder\WaldoNAVPad'
 $BackupPath = 'C:\Users\Administrator\Dropbox\GitHub\Waldo.NAV\WaldoNAVPad'
 
 
-$CreatedITems = Backup-NAVApplicationObjects -BackupOption OnlyModified -ServerInstance DynamicsNAV90 -BackupPath $BackupPath -Name $Name
+$CreatedITems = Backup-NAVApplicationObjects `                    -BackupOption OnlyModified `                    -ServerInstance $DEVInstance `                    -BackupPath $BackupPath `                    -Name $Name `                    -NavAppOriginalServerInstance $ORIGInstance `                    -NavAppWorkingFolder $WorkingFolder 
 
-$CreatedITems |where Name -like '*.txt' | Split-NAVApplicationObjectFile -Destination "$BackupPath\Split\"
+$CreatedITems |where Name -like '*.txt' | Split-NAVApplicationObjectFile -Destination "$BackupPath\Split\" -Force
 
 start $BackupPath
