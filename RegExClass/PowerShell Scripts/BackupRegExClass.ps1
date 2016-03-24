19f6611f6b49cdf9747ccd72e3ab73999cc27005
waldo <github@waldo.be>
2016-03-24 14:23:18 +0100

    RegEx Class in NAV
    Just trying things out just yet ..

---
+++ RegExClass/PowerShell Scripts/BackupRegExClass.ps1
@@ -0,0 +1,11 @@
+﻿$Name = 'RegExClass'
+$DEVInstance = 'Regex_DEV'
+$ORIGInstance = 'Regex_ORIG'
+$WorkingFolder = 'C:\_Workingfolder\RegExClass'
+$BackupPath = 'C:\Users\Administrator\Dropbox\GitHub\Waldo.NAV\RegExClass'
+
+
+$CreatedITems = Backup-NAVApplicationObjects `                    -BackupOption OnlyModified `                    -ServerInstance $DEVInstance `                    -BackupPath $BackupPath `                    -Name $Name `                    -NavAppOriginalServerInstance $ORIGInstance `                    -NavAppWorkingFolder $WorkingFolder 
+
+
+start $BackupPath
