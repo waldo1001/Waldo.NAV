43db12495ace74936344e2f0fc500224de402142
waldo <github@waldo.be>
2016-09-08 10:36:58 +0200

    WaldoNAVPad RTM
    First version ready to be released to the public (at least that's what I
    think :-))

--- WaldoNAVPad/PowerShell Scripts/5_BackupApp.ps1
+++ WaldoNAVPad/PowerShell Scripts/5_BackupApp.ps1
@@ -1,7 +1,7 @@
 ﻿# Import settings
 . (Join-Path $PSScriptRoot '_Settings.ps1') -ErrorAction Stop
 
-$CreatedITems = Backup-NAVApplicationObjects `                    -BackupOption OnlyModified `                    -ServerInstance $ModifiedServerInstance `                    -BackupPath $BackupPath `                    -Name $AppName `                    -NavAppOriginalServerInstance $OriginalServerInstance `                    -NavAppWorkingFolder $NavAppWorkingFolder
+$CreatedITems = Backup-NAVApplicationObjects `                    -BackupOption OnlyModified `                    -ServerInstance $ModifiedServerInstance `                    -BackupPath $BackupPath `                    -Name $AppName `                    -NavAppOriginalServerInstance $OriginalServerInstance `                    -NavAppWorkingFolder $NavAppWorkingFolder `                    -CompleteReset
 
 
 start $BackupPath
