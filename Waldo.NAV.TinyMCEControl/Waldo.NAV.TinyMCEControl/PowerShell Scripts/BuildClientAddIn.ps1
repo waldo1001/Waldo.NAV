a0c7a1eb2552168f3dca6d1d1ae1bacf59113a87
waldo <github@waldo.be>
2016-09-16 12:52:33 +0200

    WaldoNAVPad HTML: Added creating of Extension-ready zip to PS script

--- Waldo.NAV.TinyMCEControl/Waldo.NAV.TinyMCEControl/PowerShell Scripts/BuildClientAddIn.ps1
+++ Waldo.NAV.TinyMCEControl/Waldo.NAV.TinyMCEControl/PowerShell Scripts/BuildClientAddIn.ps1
@@ -3,16 +3,25 @@
 
 #Update Resources
 $ResourceFile = (Join-Path $ProjectFolder 'Resources.zip')
+$ExtensionResourceFile = (Join-Path $ProjectFolder 'ExtensionResources.zip')
 if (Test-Path $ResourceFile){Remove-Item -Path $ResourceFile -Force}
 
+#Resource for AddIn in NAV
 $ResourceItems = @()
 $ResourceItems += Get-Item (Join-Path $ProjectFolder 'Manifest.xml') -ErrorAction Stop
 $ResourceItems += Get-Item (Join-Path $ProjectFolder 'Script') -ErrorAction SilentlyContinue
 $ResourceItems += Get-Item (Join-Path $ProjectFolder 'StyleSheet') -ErrorAction SilentlyContinue
 $ResourceItems += Get-Item (Join-Path $ProjectFolder 'Image') -ErrorAction SilentlyContinue
-
 $ResourceItems | 
     Create-ZipFileFromPipedItems `        -zipfilename $ResourceFile
+
+#Resource for Extensions
+$ExtensionItems = @()
+$ExtensionItems += $ResourceItems
+$ExtensionItems += Get-ChildItem (join-path $ProjectFolder 'bin' ) -Recurse -Filter '*.dll' -Exclude '*Microsoft*'
+$ExtensionItems | 
+    Create-ZipFileFromPipedItems `        -zipfilename $ExtensionResourceFile
+
 
 $MyAddIn = 
     Get-NAVAddIn `        -ServerInstance $ServerInstance `        -AddInName $AddInName `        -ErrorAction SilentlyContinue
